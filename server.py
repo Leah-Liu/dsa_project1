87cb23a196cb44ba08cf1dd170c71940c24d9b24
Leah-Liu <leahliu61@gmail.com>
2017-10-15 20:57:45 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
+import sys, os, time
 import socket
 import threading
-import time
-import sys, os
 import pickle
 import Event
 from dateutil import tz
+
+'''
+DECLARATION OF GLOBAL VARIABLES
+'''
 # Read in from hosts.txt to config NODE_ID and 
 # cmd line will be like "python2 server.py hosts.txt 1", which specifies hosts file to open and my_NODE_ID
-
 hosts_file = sys.argv[1]
 NODE_ID = int(sys.argv[2])
-MY_PORT = 0
+MY_PORT = 1
 count_num = 0
 id_ports = {}
 destination_ips = {}
@@ -21,27 +23,18 @@ with open(hosts_file) as file:
         id_ports[int(parsed[1])] = int(parsed[3])
         destination_ips[int(parsed[1])] = parsed[2]
         # found my configuration
-        print(destination_ips)
+        
         if int(parsed[1]) == NODE_ID:
             my_IP, MY_PORT = parsed[2], parsed[3]
-    print(id_ports)
-    print("NODE_ID: ", NODE_ID, "IP: ", my_IP, "PORT: ", MY_PORT )
+    print("NODE_ID: ",  NODE_ID, "IP: ", my_IP, "PORT: ", MY_PORT )
     
 
-# Global Variables ---------------------------------------------------------------
-# SEPARATOR = ","
-# Global variables to define server socket
 Lock = threading.Lock()
-# id_ports = {1:8081, 2:8082, 3:8083}
 
-# Global variables to define log dictionary and partial log dictionary
-# V = set()   #local copy for all events
-
-# Bring back local Partial Log and dictionary if possible
-# backup = str(NODE_ID) + '_log.p'
+# Bring back local records if exists for current site
 if os.path.isfile(str(NODE_ID) + '_log.p'):
-	(PL, blockInformation, tweetInformation, T, clock) = pickle.load(open(str(NODE_ID) + '_log.p', "rb" ))
-
+    (PL, blockInformation, tweetInformation, T, clock) = pickle.load(open(str(NODE_ID) + '_log.p', "rb" ))
+# Otherwise, initialize as global variables
 else:
     # Partial log
     PL = set()
@@ -52,12 +45,13 @@ else:
     # 2d time matrix
     # count_num is how many lines are there in the hosts.txt file
     T = [[0 for i in xrange(count_num)] for i in xrange(count_num)] 
-    clock = 0   #time
+    #time
+    clock = 0   
             
 
-
-
-# End of global variables --------------------------------------------------------
+'''
+PREPARE FUNCTIONS
+'''
 
 
 def hasRec(T, event, siteId):
@@ -71,7 +65,6 @@ def get_NE(NP):
     return NE
 
 
-
 def exists_unblocked(block, set_events):
     for e in set_events:
         if block[0] == e[0] and block[1] == e[1]:
@@ -80,14 +73,10 @@ def exists_unblocked(block, set_events):
 
 
 def update_blockInformation(NE):
-    print("entered update_blockInformation")
+    # print("entered update_blockInformation")
     tmp = set()
     global blockInformation
     block_set = list(blockInformation) + [(int(n.node), int(n.content)) for m,n in NE if n.op == "block"]
-    
-    print("Block_set:")
-    print(block_set)
-    
     unblock_set = [(int(n.node), int(n.content)) for m,n in NE if n.op == "unblock"]
     
     for e in block_set:
@@ -95,33 +84,33 @@ def update_blockInformation(NE):
         if result is not None:
             unblock_set.remove(result)
             continue
-            # break
+            
         else:
             tmp.add((e[0], e[1]))
-            # tmp.add(e)
+            
     blockInformation = tmp
-    # return blockInformation
+    
     
     
 def update_tweetInformation(NE):
-    print("Entered update_tweetInforamtion")
+    # print("Entered update_tweetInforamtion")
     global tweetInformation
     for a, e in NE:
         if e.op == 'tweet':
             tweetInformation.add(e)
-    # return tweetInformation
+    
     
 
 
 def update_T(other_NODE_ID, other_T):
-    print("Entered update_T")
+    # print("Entered update_T")
     global T
     for k in xrange(count_num):
         T[NODE_ID-1][k] = max(T[NODE_ID-1][k], other_T[other_NODE_ID-1][k])
     for k in xrange(count_num):
-	    for l in xrange(count_num):
-	        T[k][l] = max(T[k][l], other_T[k][l])
-    # return T
+        for l in xrange(count_num):
+            T[k][l] = max(T[k][l], other_T[k][l])
+    
 
     
 def acknowledge_by_other(e):
@@ -131,16 +120,12 @@ def acknowledge_by_other(e):
     return True
     
 
-'''
-INCOMPLETE!!!!
-'''
+
 def update_PL(NE):
-    print("Entered updated_PL")
+    # print("Entered updated_PL")
     global PL
     PL = set([(a, e) for (a, e) in (PL | NE) if not acknowledge_by_other(e)])
-    # return PL
-
-
+    
 
 
 def sentMsgToOtherSites():
@@ -148,14 +133,12 @@ def sentMsgToOtherSites():
     NP = {eR | eR in PL[i] and !hasRec(T[i],eR,j)}
     send msg[i][j], T[i], NP to p[j]
     '''
-    # Make sure we do not send events individually, could break casaulity
-    # We must create the set (NP), then send that set as a whole
-    # Use pickle to unpack this as a set of event records
+    
     NP = set()
     sentNodes = set()
     global blockInformation
     global NODE_ID
-    print("entered sentMsgToOtherSites() function")
+    # print("entered sentMsgToOtherSites() function")
     for a,e in PL:
         for node in id_ports.keys():
             if node == NODE_ID:
@@ -165,20 +148,23 @@ def sentMsgToOtherSites():
             if not hasRec(T, e, node):
                 NP.add((a, e))
                 sentNodes.add(node)
-    print(sentNodes)
-    print(NP)
+    # print(sentNodes)
+    # print(NP)
     for portNum in sentNodes:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #host = socket.gethostname()
         port = id_ports[portNum]
         try:
             sock.connect((destination_ips[portNum], port))
+            # Use pickle to unpack this as a set of event records
             msgElem = (NODE_ID, T, NP)
             msg = pickle.dumps(msgElem)
             sock.send(msg)
             sock.close()
         except socket.error as e:
             print("[Error] Failed to sent event log: %s to site: %d", e, portNum)
+
+
 
 def tweet(message, NODE_ID):
     """
@@ -187,7 +173,7 @@ def tweet(message, NODE_ID):
     if !hasRec(time, event, j)
     """
     #Lock.acquire()
-    print("Entered tweet function")
+    # print("Entered tweet function")
     global clock
     clock += 1
     T[NODE_ID-1][NODE_ID-1] = clock
@@ -196,6 +182,8 @@ def tweet(message, NODE_ID):
     tweetInformation.add(tweetEvent)
     sentMsgToOtherSites()
     #Lock.release()
+
+
 
 def block(NODE_ID, userId):
     """
@@ -221,6 +209,8 @@ def block(NODE_ID, userId):
         blockInformation.add((NODE_ID, int(userId)))
         #Lock.release()
 
+
+
 def unblock(NODE_ID, userId):
     #input validation
     if int(userId) not in id_ports.keys():
@@ -237,35 +227,43 @@ def unblock(NODE_ID, userId):
         
         blockInformation.remove((NODE_ID, int(userId)))
 
+
 def view():
     """
     display timeline of entire set of tweets
     """
-    print("entered view() function")
+    # print("entered view() function")
     for e in sorted(tweetInformation, key=getKey, reverse=True):
     # sort based on time
         # if I'm not blocked by the server who created the event
+        central = getKey(e)
         if (e.node, NODE_ID) not in blockInformation and e.op == "tweet":
-            print("Server: {}, Tweet msg: {}, UTC time: {}\n".format(e.node, e.content, e.utc))
+            print("Server: {}, Tweet msg: {}, Local time: {}\n".format(e.node, e.content, central))
 
 def getKey(item):
-   
     utc = item.utc
     from_zone = tz.tzutc()
     to_zone = tz.tzlocal()
     utc = utc.replace(tzinfo=from_zone)
     central = utc.astimezone(to_zone)
     return central    
+
+
     
 def showPL():
     for pl in PL:
         print("Type: {}, Server: {},  Operation: {},  Content: {}, UTC time: {}\n".format(pl[0], pl[1].node, pl[1].op, pl[1].content, pl[1].time))
 
+
 def showT():
-    print(T)
+    for i in range(count_num):
+        print(T[i])
+    # print(T)
+
 
 def show_lockInformation():
     print(blockInformation)
+ 
     
 def print_message(threadName, message):
     print (threadName, " received: ", message)
@@ -278,23 +276,20 @@ class threadRemote(threading.Thread):
         self.name = name
         self.description = description
 
-    def process(self, c, addr): # UNCALLED FUNCTION
+    def process(self, c, addr): 
         c.send('Thank you for connecting')
         message = c.recv(4096).decode("ascii")
         #Lock.acquire()
-        # print_message(self.name, message)
-        #Lock.release()
+        
         c.close()
         
-        # Upon receipt, loads pickle
-        # (other_ID, other_T, NP) = pickle.loads(message)
         
-        # DO V UPDATE, PL UPDATE, BLOCKINFORMATION UPDATE UPON RECEIVING
+        
     def run(self):
         print("Starting" + self.name)
         
         remoteSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        host = socket.gethostname()
+        #host = socket.gethostname()
         remoteSocket.bind(('', int(MY_PORT)))
         remoteSocket.listen(10) #wait for client connection
         while True:
@@ -309,29 +304,26 @@ class threadRemote(threading.Thread):
             #process_thread.start()
             print( 'Got connection from', addr)
             message = c.recv(4096)
-            print(message)
+            # print(message)
             
             #Lock.acquire()
             
+            # Upon receipt, loads pickle
             (other_NODE_ID, other_T, NP) = pickle.loads(message)
             other_NODE_ID = int(other_NODE_ID)
             c.close()
+            
+            
             # UPON RECEIPT, UPDATE NE, blockInformation, tweetInformation and T
-            
-            
             NE = get_NE(NP)
-            print("NE: ")
-            print(NE)
+            # print("NE: ")
+            # print(NE)
             
             global blockInformation
             update_blockInformation(NE)
-            print("blockInformation printing after update: ")
-            print(blockInformation)
             
             global tweetInformation
             update_tweetInformation(NE)
-            print("tweetInformation printing after update: ")
-            print(tweetInformation)
             
             global T
             update_T(other_NODE_ID, other_T)
@@ -339,15 +331,9 @@ class threadRemote(threading.Thread):
             global PL
             update_PL(NE)
             
-            # Save in local in case of crash
-        #     if os.path.isfile(str(NODE_ID) + '_log.p'):
-	       # (PL, blockInformation, tweetInformation, T, clock) = pickle.load(open(str(NODE_ID) + '_log.p', "rb" ))
+            # Save in local disk
             pickle.dump((PL, blockInformation, tweetInformation, T, clock), open(str(NODE_ID) + '_log.p', "wb" ))
-            
-            # Note: Upon receipt of message, verify if you need to update your blockinformation
-            # It is possible for someone to send you another persons block information
-        
-            
+
             #Lock.release()
             
 
@@ -381,37 +367,40 @@ class threadLocal(threading.Thread):
             
         while True:
             inputData = raw_input("Please enter the operation: ")
+            
             if inputData.strip() == "":
                 continue
+            
             parsed = inputData.split(" ")
+            
             cmd = str(parsed[0]).strip()
-            # content is a string, either the tweet msg or (un)block usrname.
+
             content = " ".join(parsed[1:]) 
             
-            if cmd == "view":
+            if cmd.lower() == "view":
                 view()
             
-            elif cmd == "tweet":
+            elif cmd.lower() == "tweet":
                 print(content)
                 tweet(content, NODE_ID)
             
-            elif cmd == "block":
+            elif cmd.lower() == "block":
                 block(NODE_ID,content)
             
-            elif cmd == "unblock":
+            elif cmd.lower() == "unblock":
                 unblock(NODE_ID, content)
             
-            elif cmd == "showPL":
+            elif cmd.lower() == "showpl":
                 showPL()
                 
-            elif cmd == "showT":
+            elif cmd.lower() == "showt":
                 showT()
             
-            elif cmd == "showBlock":
+            elif cmd.lower() == "showblock":
                 show_lockInformation()
             
                 
-            elif cmd == "menu":
+            elif cmd.lower() == "menu":
                 print("Please enter\n\t - tweet your_msg to tweet a message.\
                         \n\t - block user_name to block a user.\
                         \n\t - unblock user_name to unblock a user.\
@@ -422,13 +411,13 @@ class threadLocal(threading.Thread):
                         \n\t - menu to show menu.\
                         \n\t - quit to exit.")
             
-            elif cmd == "quit":
-                sys.exit("Exiting...")
+            elif cmd.lower() == "quit":
+                sys.exit()
             else:
                 print("invalid command")
-        print_message(self.name, "this is message")
+        
         Lock.release()
-        #print "Exiting" + self.name
+
 
 def main():
     local = threadLocal(1, "threadLocal", "handle local command")
