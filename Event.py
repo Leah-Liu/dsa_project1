e24e9851bd994a401ea2a403790c0a7f8d04e95e
Leah-Liu <leahliu61@gmail.com>
2017-10-15 20:58:46 -0400

    Update Event.py

--- Event.py
+++ Event.py
@@ -1,7 +1,3 @@
-# mn":27},"end":{"row":22,"column":28},"action":"insert","lines":["f"],"id":709}],[{"start":{"row":22,"column":28},"end":{"row":22,"column":29},"action":"insert","lines":[" "],"id":710}],[{"start":{"row":22,"column":29},"end":{"row":22,"column":30},"action":"insert","lines":["e"],"id":711}],[{"start":{"row":22,"column":30},"end":{"row":22,"column":31},"action":"insert","lin
-
-
-# blockEvent = Event.Event('block', clock, NODE_ID, str(userId))
 import datetime
 
 class Event:
